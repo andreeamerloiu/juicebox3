edfbedbbc3fe87be73267bf98e533b596c470e4f
andreeamerloiu <82194090+andreeamerloiu@users.noreply.github.com>
2023-09-28 20:17:30 -0700

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -8,7 +8,7 @@ const {
   getAllPosts,
   updatePost,
   getPostById,
-} = require('../db');
+} = require('../db').default;
 
 postsRouter.get('/', async (req, res, next) => {
   try {
@@ -98,7 +98,27 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
 });
 
 postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
-  res.send({ message: 'under construction' });
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
+  }
 });
+
 
 module.exports = postsRouter;
